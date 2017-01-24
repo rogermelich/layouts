7f3cef710601985d091ca48612d3cd55dd0c9f79
Roger Melich <rogermelich@gmail.com>
2017-01-24 16:29:03 +0100

    Add Example justify-content

---
+++ resources/views/vertical.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Document</title>
+</head>
+
+<style>
+	body {
+		background: green;
+		color: white;
+		text-align: center;
+		justify-content: center;
+	}
+
+	#block {
+		border: 1px black solid;
+		text-align: center;
+		max-width: 500px;
+		margin: 0 auto;
+	}
+</style>
+
+
+<body>
+Hello Word
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ Route::group(['middleware' => 'auth'], function () {
 
     //Please do not remove this if you want adminlte:route and adminlte:link commands to works correctly.
     #adminlte_routes
+    Route::get('vertical1', 'Vertical1Controller@index')->name('vertical1');
+
+    Route::get('vertical', 'VerticalController@index')->name('vertical');
+
     Route::get('prova', 'ProvaController@index')->name('prova');
 
 });
